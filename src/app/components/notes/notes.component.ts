1faaf0d3d1d71f0cd798312b113b31c805cb6fb2
Michael Gira <contact@michaelgira.me>
2017-02-15 13:59:43 -0600

    Try to get deepstream working

--- src/app/components/notes/notes.component.ts
+++ src/app/components/notes/notes.component.ts
@@ -1,4 +1,5 @@
 import { Component, ViewChild, OnInit } from '@angular/core';
+import * as deepstream from 'deepstream.io-client-js';
 
 @Component({
 	selector: 'mymicds-notes',
@@ -12,6 +13,9 @@ export class NotesComponent implements OnInit {
 
 	private _text: string = '';
 
+	client: any = deepstream('localhost:6020').login();
+	notesRecord: any = this.client.record.getRecord('anotherNotes');
+
 	get text() {
 		return this._text;
 	}
@@ -19,20 +23,22 @@ export class NotesComponent implements OnInit {
 	set text(value) {
 		this._text = value;
 		this.resizeEditor();
+		this.notesRecord.set('note', this._text);
 	}
 
 	constructor() { }
 
 	ngOnInit() {
 		this.text = 'test';
+		this.notesRecord.subscribe('note', value => {
+			this._text = value;
+		});
 	}
 
 	resizeEditor() {
-		// this.editor.nativeElement.style.height = '5px';
 		while (this.editor.nativeElement.scrollHeight !== this.editor.nativeElement.offsetHeight) {
 			this.editor.nativeElement.style.height = this.editor.nativeElement.scrollHeight + 'px';
 		}
-		// console.log(this.editor.nativeElement.style.height, this.editor.nativeElement.scrollHeight, this.editor.nativeElement.clientHeight, this.editor.nativeElement.offsetHeight);
 	}
 
 }
